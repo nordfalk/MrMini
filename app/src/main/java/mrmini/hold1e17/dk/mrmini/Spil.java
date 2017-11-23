55a9871fac802db958ce614b7d1059b3c087d064
Troels Just Christoffersen <troelsjc@gmail.com>
2017-11-23 16:18:49 +0100

    extending View, adding onDraw method

--- app/src/main/java/mrmini/hold1e17/dk/mrmini/Spil.java
+++ app/src/main/java/mrmini/hold1e17/dk/mrmini/Spil.java
@@ -1,13 +1,20 @@
 package mrmini.hold1e17.dk.mrmini;
 
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Spil extends AppCompatActivity {
+public class Spil extends View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spil);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
     }
 }
